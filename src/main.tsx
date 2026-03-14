880c5e23f04f5d81f6ddcf92d6a3119d0ddbf1f4
sjww1997 <sjww1997@126.com>
2026-03-14 17:16:11 +0800

    更新ant design

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router'
-import '@ant-design/v5-patch-for-react-19';
 import AuthProvider from './auth/AuthProvider'
 
 
